107aedd8d8c6111d5f0f8d18f22eb1b5945a38ce
Mohamed Abdelrazek <moh.abdelrazek2024@hotmail.com>
2024-08-18 20:38:33 +0300

    batman

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <main className="App">
+     <div>
+      <h1>Hello World</h1>
+      <p>
+        fuck you!!!
+      </p>
+     </div> 
+    </main>
+  )
+}
